886e6b3fdb4cb8ad8bda706d6a83c9a73d0dff2b
Marc Haubenstock <marc.dhaubenstock@gmail.com>
2023-02-16 21:31:47 +0100

    params

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+extern crate nalgebra as na;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use na::{OMatrix,OVector, Scalar, Dim, default_allocator::DefaultAllocator, allocator::Allocator};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+
+/**
+ * A is Dim(MxN), x,c are Dim(N), y,b are Dim(M)
+ * Returns (x (primal), y (dual))
+ */
+pub fn solve<T: Scalar, M: Dim, N: Dim>(A: &OMatrix<T,M,N>, b: &OVector<T,M>, c: &OVector<T,N>) -> (OVector<T, N>,OVector<T,M>) where  DefaultAllocator: Allocator<T, M> + Allocator<T, M, N>+ Allocator<T, N> {
+    panic!("TODO");
 }
