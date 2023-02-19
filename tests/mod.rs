d1cc1c0b31fd39627d188dc08e27b5ff5d7405cf
Marc Haubenstock <marc.dhaubenstock@gmail.com>
2023-02-19 01:11:27 +0100

    nit

--- tests/mod.rs
+++ tests/mod.rs
@@ -9,7 +9,7 @@ use linear_ip;
  * (Dual) Max 2*x_1 + 2*x_2 s.t. x_1 + x_2 <= 3 and x_1, x_2 >= 0
  */
 #[test]
-fn simple(){
+fn simple_f64(){
     let a = Vector2::<f64>::new(1.0,1.0);
     let b = Vector2::<f64>::new(2.0,2.0);
     let c = Vector1::<f64>::new(3.0);
@@ -21,4 +21,22 @@ fn simple(){
     let y2 = y[1];
     assert_relative_eq!(y1,1.5f64, epsilon  = eps);
     assert_relative_eq!(y2,1.5f64, epsilon  = eps);
+}
+
+/**
+ * (Dual) Max 2*x_1 + 2*x_2 s.t. x_1 + x_2 <= 3 and x_1, x_2 >= 0
+ */
+#[test]
+fn simple_f32(){
+    let a = Vector2::<f32>::new(1.0,1.0);
+    let b = Vector2::<f32>::new(2.0,2.0);
+    let c = Vector1::<f32>::new(3.0);
+    let eps: f32 = 1e-5;
+    let max_it = 100;
+
+    let (_,y) = linear_ip::solve(&a, &b, &c, eps, max_it);
+    let y1 = y[0];
+    let y2 = y[1];
+    assert_relative_eq!(y1,1.5f32, epsilon  = eps);
+    assert_relative_eq!(y2,1.5f32, epsilon  = eps);
 }
