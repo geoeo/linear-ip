3a0df263cef01307c55befbb8d4a7202c5373495
Marc Haubenstock <marc.dhaubenstock@gmail.com>
2023-04-03 16:53:16 +0200

    added dyn method

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 extern crate nalgebra as na;
 
-use na::{OMatrix,OVector, Scalar, Dim, DimName, default_allocator::DefaultAllocator, allocator::Allocator, convert, RealField, DimMin, DimSub, Const};
+use na::{OMatrix, OVector, DMatrix, DVector, Scalar, Dim, DimName, default_allocator::DefaultAllocator, allocator::Allocator, convert, RealField, DimMin, DimSub, Const};
 
 
 /**
@@ -49,6 +49,72 @@ pub fn solve<T, M, N>(A: &OMatrix<T,M,N>, b: &OVector<T,M>, c: &OVector<T,N>, ep
         let mut step_dual = max_val;
 
         for i in 0..N::USIZE {
+            let dx_i = d_x[i];
+            let ds_i = d_s[i];
+
+            if dx_i < T::zero() {
+                let v = -x[i]/dx_i;
+                step_primal = step_primal.min(v);
+            }
+
+            if ds_i < T::zero() {
+                let v = -s[i]/ds_i;
+                step_dual = step_dual.min(v);
+            }
+        }
+
+        let step_max = step_primal.min(step_dual);
+        let step = one.min(theta*step_max);
+
+        x = x+d_x.scale(step);
+        y = y+d_y.scale(step);
+        s = s+d_s.scale(step);
+        k = k+1;
+    }
+   
+    (x,y)
+}
+
+#[allow(non_snake_case)]
+pub fn solve_dyn<T>(A: &DMatrix<T>, b: &DVector<T>, c: &DVector<T>, eps: T, theta: T, gamma: T, max_it: usize) -> (DVector<T>, DVector<T>) 
+    where T: Scalar + RealField + Copy{
+    let one: T = convert(1.0);
+    let max_val = T::max_value().unwrap();
+    let svd_eps: T = convert(1e-20);
+    let N = A.ncols();
+    let M = A.nrows();
+
+    let A_transpose = A.transpose();
+    let mut gamma_mu = DVector::<T>::from_element(N, one);
+    let mut x = DVector::<T>::from_element(N, one);
+    let mut s = DVector::<T>::from_element(N, one);
+    let mut y = DVector::<T>::zeros(M);
+    let mut r_primal = DVector::<T>::from_element(M, max_val);
+    let mut r_dual = DVector::<T>::from_element(N, max_val);
+    let mut k = 0;
+    let mut S_inv = DMatrix::<T>::zeros(N,N);
+    let mut X = DMatrix::<T>::zeros(N,N);
+    let n : T = convert(N as f64);
+
+    while k < max_it && (r_primal.norm() > eps || r_dual.norm() > eps || x.dot(&s) > eps) {
+        r_primal = b - A*(&x);
+        r_dual = c - (&A_transpose)*(&y) - (&s);
+        let mu = x.dot(&s)/n;
+        for i in 0..N {
+            S_inv[(i,i)] = one/s[i];
+            X[(i,i)] = x[i];
+            gamma_mu[i] = gamma*mu;
+        }
+        let M = A*(&S_inv)*(&X)*(&A_transpose);
+        let r = b + A*(&S_inv)*((&X)*(&r_dual) - (&gamma_mu));
+        let d_y = M.svd(true,true).solve(&r, svd_eps).expect("direction y failed");
+        let d_s = (&r_dual) - (&A_transpose)*(&d_y);
+        let d_x = -(&x) + (&S_inv)*((&gamma_mu)-(&X)*(&d_s));
+
+        let mut step_primal = max_val;
+        let mut step_dual = max_val;
+
+        for i in 0..N {
             let dx_i = d_x[i];
             let ds_i = d_s[i];
 
--- tests/mod.rs
+++ tests/mod.rs
@@ -2,7 +2,7 @@ extern crate nalgebra as na;
 #[macro_use]
 extern crate approx;
 
-use na::{Vector2,Vector1};
+use na::{Vector2,Vector1,DVector, DMatrix};
 use linear_ip;
 
 /**
@@ -15,10 +15,30 @@ fn simple_f64(){
     let c = Vector1::<f64>::new(3.0);
     let eps: f64 = 1e-10;
     let theta : f64 = 0.95;
-    let gamma : f64 = 0.05;
+    let gamma : f64 = 0.1;
     let max_it = 10;
 
     let (_,y) = linear_ip::solve(&a, &b, &c, eps, theta, gamma, max_it);
+    let y1 = y[0];
+    let y2 = y[1];
+    assert_relative_eq!(y1,1.5f64, epsilon  = eps);
+    assert_relative_eq!(y2,1.5f64, epsilon  = eps);
+}
+
+/**
+ * (Dual) Max 2*x_1 + 2*x_2 s.t. x_1 + x_2 <= 3 and x_1, x_2 >= 0
+ */
+#[test]
+fn simple_f64_dyn(){
+    let a = DMatrix::<f64>::from_vec(2,1, vec![1.0,1.0]);
+    let b = DVector::<f64>::from_vec(vec![2.0,2.0]);
+    let c = DVector::<f64>::from_vec(vec![3.0]);
+    let eps: f64 = 1e-10;
+    let theta : f64 = 0.95;
+    let gamma : f64 = 0.1;
+    let max_it = 10;
+
+    let (_,y) = linear_ip::solve_dyn(&a, &b, &c, eps, theta, gamma, max_it);
     let y1 = y[0];
     let y2 = y[1];
     assert_relative_eq!(y1,1.5f64, epsilon  = eps);
@@ -35,10 +55,30 @@ fn simple_f32(){
     let c = Vector1::<f32>::new(3.0);
     let eps: f32 = 1e-10;
     let theta : f32 = 0.95;
-    let gamma : f32 = 0.05;
+    let gamma : f32 = 0.1;
     let max_it = 10;
 
     let (_,y) = linear_ip::solve(&a, &b, &c, eps, theta, gamma, max_it);
+    let y1 = y[0];
+    let y2 = y[1];
+    assert_relative_eq!(y1,1.5f32, epsilon  = eps);
+    assert_relative_eq!(y2,1.5f32, epsilon  = eps);
+}
+
+/**
+ * (Dual) Max 2*x_1 + 2*x_2 s.t. x_1 + x_2 <= 3 and x_1, x_2 >= 0
+ */
+#[test]
+fn simple_f32_dyn(){
+    let a = DMatrix::<f32>::from_vec(2,1,vec![1.0,1.0]);
+    let b = DVector::<f32>::from_vec(vec![2.0,2.0]);
+    let c = DVector::<f32>::from_vec(vec![3.0]);
+    let eps: f32 = 1e-10;
+    let theta : f32 = 0.95;
+    let gamma : f32 = 0.1;
+    let max_it = 10;
+
+    let (_,y) = linear_ip::solve_dyn(&a, &b, &c, eps, theta, gamma, max_it);
     let y1 = y[0];
     let y2 = y[1];
     assert_relative_eq!(y1,1.5f32, epsilon  = eps);
